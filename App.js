2f5316145c251290938449cbed1ad22f8348a994
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-06 10:13:42 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.btnStyle}>
+        <Text style={styles.textStyle}>Upload Imgae</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnStyle: {
+    backgroundColor: 'blue',
+    height: 48,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    paddingHorizontal: 16,
+  },
+});
